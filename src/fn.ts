483dd9372a84a3ac9e95ceac3bb57c6beaa1f36e
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2025-03-16 16:11:21 +0200

    fix: fix decorator
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- src/fn.ts
+++ src/fn.ts
@@ -1,21 +1,8 @@
 type FnOptions = { description: string };
 
-export function fn(
-  options?: FnOptions
-): (target: any, context: ClassMethodDecoratorContext) => any;
+export function fn(options?: FnOptions): any;
 export function fn(target: any, context: ClassMethodDecoratorContext): any;
 export function fn(
-  targetOrOptions?: any | FnOptions,
+  targetOrOptions?: FnOptions,
   context?: ClassMethodDecoratorContext
-) {
-  // If called as @fn({ description: '...' })
-  if (targetOrOptions && !context) {
-    return function (target: any, context: ClassMethodDecoratorContext) {
-      // Here you can use targetOrOptions.description
-      return target;
-    };
-  }
-
-  // If called as @fn
-  return targetOrOptions;
-}
+) {}
