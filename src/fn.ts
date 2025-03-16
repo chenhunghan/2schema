9d897696e1b173bfc92b0c45519c1ea8f6a69266
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2025-03-16 16:33:30 +0200

    feat: improve typing of exported decorator
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- src/fn.ts
+++ src/fn.ts
@@ -1,3 +1,20 @@
-type FnOptions = { description: string };
+type FnOptions = {
+  /**
+   * The description of function, what does it do.
+   */
+  description: string,
+  /**
+   * The description of parameters of function
+   */
+  parameters: Map<string, { description: string }>,
+  /**
+   * output schema with function.strict = true|false, default is true
+   */
+  strict?: boolean,
+  /**
+   * output schema with function.additionalProperties = true|false, default is false
+   */
+  additionalProperties?: boolean,
+};
 
 export function fn(options?: FnOptions): any {};
