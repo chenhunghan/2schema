aeedd21626db8883fff507f342c37ca11cd5f44a
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2025-03-16 16:12:55 +0200

    fix: simple fn decorator types
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- src/fn.ts
+++ src/fn.ts
@@ -1,8 +1,3 @@
 type FnOptions = { description: string };
 
-export function fn(options?: FnOptions): any;
-export function fn(target: any, context: ClassMethodDecoratorContext): any;
-export function fn(
-  targetOrOptions?: FnOptions,
-  context?: ClassMethodDecoratorContext
-) {}
+export function fn(options?: FnOptions): any {};
